131ce577b8ca11c92aabec108b10392ac1a89692
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-10-23 14:18:17 -0700

    Update Weird-string-case.js

--- Weird-string-case.js
+++ Weird-string-case.js
@@ -38,3 +38,15 @@ function toWeirdCase(string) {
 }
 
 // or
+
+const toWeirdCase = function (string) {
+  return string.toLowerCase().split(' ').map(mapWord).join(' ');
+};
+
+const mapWord = function (word) {
+  return word.split('').map(mapCharacter).join('');
+};
+
+const mapCharacter = function (letter, index) {
+  return (index % 2 === 0) ? letter.toUpperCase() : letter;
+}
